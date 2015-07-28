42fa55bdf2c72a26af7f63de67d65001b200bb48
vanillasky59 <requiem-sky1>
2015-07-28 12:23:12 +0300

    w

--- tests/sign_in.py
+++ tests/sign_in.py
@@ -1,8 +1,3 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
 __author__ = 'Olga'
 
 import os
